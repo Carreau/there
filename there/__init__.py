d7b56c846364228f8c796b1019c80ac9d4f0cb36
Matthias Bussonnier <bussonniermatthias@gmail.com>
2018-10-31 14:56:17 -0700

    print funct

--- there/__init__.py
+++ there/__init__.py
@@ -5,7 +5,7 @@ Print current file and line number
    print(there)
 """
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 import sys
 import inspect
@@ -30,28 +30,33 @@ class There(types.ModuleType):
     """
 
     __version__ = __version__
-
+    def __init__(self, *args, **kwargs):
+        self.level = 0
+        super().__init__(*args, **kwargs)
 
     @property
     def Undefined(self):
         return self
 
     def __str__(self):
-        cf = inspect.currentframe().f_back
-        return '{}:{}'.format(compress(cf.f_code.co_filename), cf.f_lineno)
+        cf = sys._getframe( self.level+ 1 )
+        return '{}:{} in {}'.format(compress(cf.f_code.co_filename), cf.f_lineno, cf.f_code.co_name)
 
     def __repr__(self):
         return str(self)
-        cf = inspect.currentframe().f_back
+        cf = sys._getframe( self.level+ 1 )
         return '<there {}:{}>'.format(compress(cf.f_code.co_filename), cf.f_lineno)
 
     def print(self, *args, **kwargs):
-        cf = inspect.currentframe().f_back
-        _print('{}:{}'.format(compress(cf.f_code.co_filename), cf.f_lineno), '|',*args, **kwargs)
+        cf = sys._getframe( self.level+ 1 )
+        _print('{}:{} in '.format(compress(cf.f_code.co_filename), cf.f_lineno, cf.f_code.co_name), '|',*args, **kwargs)
 
     def syslogprint(self, *args):
         cf = inspect.currentframe().f_back
         syslog.syslog(syslog.LOG_NOTICE, '{}:{}'.format(compress(cf.f_code.co_filename), cf.f_lineno) + '|' + ' '.join([str(x) for x in args]).replace('\n','\\n'))
+
+    def FUNC( back = 0):
+        return sys._getframe( self.level+ 1 ).f_code.co_name
 
     def FILE(self):
         cf = inspect.currentframe().f_back
